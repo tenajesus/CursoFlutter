f0139b617d74675ea17de8e325726eb14904ee72
tenajesus <tenajesus@hotmail.com>
2019-06-06 09:57:31 -0500

    Se ha empezado a trabajar con Extensiones Stateful

--- startup_namer_stateful/lib/main.dart
+++ startup_namer_stateful/lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+
 void main() => runApp(MyApp());
+
+class RandomWordsState extends State<RandomWords>{
+  //Todo Add build() method
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget{
+@override
+RandomWordsState createState() => RandomWordsState();
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+    
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           // child: Text('Instructor: ITIC. Jesús Salvador Rodriguez Cardiel'),
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
